fc6c276ac2dd55ae0e9db06c22551b3d2f967062
Ryan Sariego <rsariego@fastmail.com>
2023-05-27 09:09:13 -0400

    user data properly seeding Profile component

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { getCurrentUserProfile } from '../../server/api/spotify';
 
 const Profile = () => {
 
-    const { auth } = useSelector(state => state);
+    const { auth, users } = useSelector(state => state);
+
+    const user = users.find(user => user.spotifyId === auth.id);
+
+    if(!user){
+        return null;
+    }
 
     const spotUsername = auth.display_name;
     const spotEmail = auth.email;
-
-    if(!auth){
-        return null;
-    }
+    const image = auth.images[0].url;
     
     return(
         <div>
-            <h1>Profile - {spotUsername}</h1>
-            <h1>{spotEmail}</h1>
+            <h1>Profile - { spotUsername }</h1>
+            <h1>{ spotEmail }</h1>
+            <img src={ image }/>
             <Link to='/'>GO BACK</Link>
         </div>
     )
