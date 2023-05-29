a4eac1f59970758fd115e687ccd6325d80f4be6e
Mauricio Toscano <toscano95@gmail.com>
2023-05-29 12:10:47 -0400

    merging Ryan's code with the navbar component

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -5,14 +5,72 @@ import { updateUser, fetchUsers, fetchSpotUser } from '../store';
 import Switch from '@mui/material/Switch';
 
 const Profile = () => {
+
+    const { auth, users, playlists } = useSelector(state => state);
+    const [discover, setDiscover] = useState(null);
+    const [selectedUser, setSelectedUser] = useState([]);
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const user = users.find(user => user.spotifyId === auth.id);
+        if(user){
+            setSelectedUser(user);
+            setDiscover(user.discoverPlaylists)
+        } 
+    }, [users])
+
+    useEffect(() => {
+        if(selectedUser){
+        dispatch(updateUser({id: selectedUser.id, discoverPlaylists: discover}));   
+        }
+    }, [discover])
+
+
+    const discoverToggle = () => {
+        setDiscover((current) => !current);
+    }
+
+    if(!selectedUser){
+        return null;
+    }
+
+    if(!playlists){
+        return null;
+    }
+    if(!auth){
+        return null;
+    }
+    const spotUsername = auth.display_name;
+    const spotEmail = auth.email;
+    const image = auth.images[0].url;
+    const followerCount  = auth.followers.total;
+    
+    return(
+        <div>
+            <h1><span className='prof-title'>Profile</span>: ({ spotUsername.toUpperCase() })</h1>
+            <h1><span className='prof-title'>Email</span>: { spotEmail }</h1>
+            <h1><span className='prof-title'>Follower Count</span>: { followerCount }</h1>
+            <h1><span className='prof-title'>Playlist Count</span>: { playlists.length }</h1>
+            <h1><span className='prof-title'>Discover Playlists?</span> 
+            <Switch checked={discover} onClick={() => discoverToggle()}/></h1>
+            <img src={ image }/>
+        </div>
+    )
+}
+export default Profile;
+
+
+/* MT 5,28
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useParams } from 'react-router-dom';
+import { updateUser, fetchUsers, fetchSpotUser } from '../store';
+import Switch from '@mui/material/Switch';
+
+const Profile = () => {
   const { auth, users, playlists } = useSelector((state) => state);
   const [discover, setDiscover] = useState("");
-
-/* 5.28 Ryan's code
-  const [discover, setDiscover] = useState(null);
-*/
-
-  const [selectedUser, setSelectedUser] = useState([]);
 
   const dispatch = useDispatch();
 
@@ -37,31 +95,6 @@ const Profile = () => {
   if (!user) {
     return null;
   }
-
-  /* 5.28 Ryan's code
-useEffect(() => {
-        const user = users.find(user => user.spotifyId === auth.id);
-        if(user){
-            setSelectedUser(user);
-            setDiscover(user.discoverPlaylists)
-        } 
-    }, [users])
-
-    useEffect(() => {
-        if(selectedUser){
-        dispatch(updateUser({id: selectedUser.id, discoverPlaylists: discover}));   
-        }
-    }, [discover])
-
-    const discoverToggle = () => {
-        setDiscover((current) => !current);
-    }
-
-    if(!selectedUser){
-        return null;
-    }
-*/
-
 
   if (!playlists) {
     return null;
@@ -99,17 +132,9 @@ useEffect(() => {
   );
 };
 
-/* 5.28 MT code 
-
-      <h1>
-        <span className="prof-title">Discover Playlists?</span>{' '}
-        {user.discoverPlaylists.toString()}{' '}
-        <button onClick={discoverToggle}>{discover ? 'Toggle off' : 'Toggle on'}</button>
-      </h1>
-
-*/
 
 export default Profile;
+*/
 
 
 /*
