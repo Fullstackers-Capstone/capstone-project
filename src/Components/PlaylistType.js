f084ea07691079d4007dee09a72fe0d21077699a
Mauricio Toscano <toscano95@gmail.com>
2023-06-06 21:59:09 -0400

    changes

--- src/Components/PlaylistType.js
+++ src/Components/PlaylistType.js
@@ -13,13 +13,17 @@ const PlaylistType = () => {
             <h3>AI Prompt</h3>
             <span className='pl-type-desc'>Describe what you want a playlist for and an AI will make the perfect mix.</span>
           </div>
-          <div className="option">
-            <h3>Genres</h3>
-            <span className='pl-type-desc'>Blend up to five genres to create a playlist.</span>
+          <div className="option" >
+            <h3>Friends of Artists</h3>
+            <span className='pl-type-desc'>Describe what you want a playlist for and an AI will make the perfect mix.</span>
           </div>
           <div className="option">
             <h3>Rewind</h3>
             <span className='pl-type-desc'>Create personalized playlists based on your listening history.</span>
+          </div>
+          <div className="option">
+            <h3>Genres</h3>
+            <span className='pl-type-desc'>Blend up to five genres to create a playlist.</span>
           </div>
         </div>
       </div>
