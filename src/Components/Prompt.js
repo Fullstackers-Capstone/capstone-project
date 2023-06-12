fd1dbc2d7eb16a00efe0f4f10d940e375808a042
“Brian <brianshanley242@gmail.com>
2023-06-12 11:49:31 -0400

    Add more tracks functionality working

--- src/Components/Prompt.js
+++ src/Components/Prompt.js
@@ -16,7 +16,7 @@ const Prompt = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [stringTopTracks, setStringTopTracks] = useState('');
   const [selectedItems, setSelectedItems] = useState([]);
-
+  const [showAllTracks, setShowAllTracks] = useState(false); // added
 
   useEffect(() => {
     async function getUserTopTracks() {
@@ -91,41 +91,40 @@ const Prompt = () => {
         <Loader />
       ) : (
         <>
-
-{testClicked && Array.isArray(jsonResponse) && jsonResponse.length > 0 && (
-  <div className="playlist-container">
-    <h2 className="playlist-header">Playlist</h2>
-    {jsonResponse.map((response, index) => (
-      <div className="playlist-item" key={index}>
-        <div className="playlist-item-info">
-          <div className="playlist-item-row">
-            <div className="playlist-item-title">{response.title}</div>
-          </div>
-          <div className="playlist-item-artist">{response.artist}</div>
-          <div className="playlist-item-checkbox">
-            <input
-              type="checkbox"
-              checked={selectedItems.includes(index)}
-              onChange={() => toggleItemSelection(index)}
-            />
-          </div>
-        </div>
-      </div>
-    ))}
-    <div className="playlist-buttons-container">
-      <button className="playlist-back-button" onClick={goBack}>
-        Back
-      </button>
-      <button
-        className="playlist-generate-button"
-        onClick={() => handleGeneratePlaylist()}
-      >
-        Generate Playlist
-      </button>
-    </div>
-  </div>
-)}
-
+          {testClicked && Array.isArray(jsonResponse) && jsonResponse.length > 0 && (
+            <div className={`playlist-container ${showAllTracks ? 'show-all' : ''}`}>
+              <h2 className="playlist-header">Playlist</h2>
+              {jsonResponse.map((response, index) => (
+                <div className="playlist-item" key={index}>
+                  <div className="playlist-item-info">
+                    <div className="playlist-item-row">
+                      <div className="playlist-item-title">{response.title}</div>
+                    </div>
+                    <div className="playlist-item-artist">{response.artist}</div>
+                    <div className="playlist-item-checkbox">
+                      <input
+                        type="checkbox"
+                        checked={selectedItems.includes(index)}
+                        onChange={() => toggleItemSelection(index)}
+                      />
+                    </div>
+                  </div>
+                </div>
+              ))}
+              <button className='styled-logout-button' onClick={() => setShowAllTracks(true)}>Add More Tracks</button> {/* added */}
+              <div className="playlist-buttons-container">
+                <button className="playlist-back-button" onClick={goBack}>
+                  Back
+                </button>
+                <button
+                  className="playlist-generate-button"
+                  onClick={() => handleGeneratePlaylist()}
+                >
+                  Generate Playlist
+                </button>
+              </div>
+            </div>
+          )}
 
           {showExamplePrompts && (
             <>
@@ -156,90 +155,160 @@ const Prompt = () => {
 export default Prompt;
 
 
+// import React, { useState, useEffect } from 'react';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { getResponse, getJSONResponse, getSpotifyURIs, wasCreated, fetchPlaylists } from '../store';
+// import Searcher from './Searcher';
+// import { getTopTracks, createPlaylist } from '../../server/api/spotify';
+// import Loader from './Loader';
 
-/* BEFORE 6/10 MT Updates
-
-
-import React, { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { getResponse, getJSONResponse, wasCreated } from '../store';
-import Searcher from './Searcher';
-import { getTopTracks, createPlaylist } from '../../server/api/spotify';
-
-const Prompt = () => {
-  const dispatch= useDispatch();
-  const { prompt, auth } = useSelector(state => state);
-  const [input,setInput] = useState('');
-  const [topTracks, setTopTracks] = useState(''); 
-  const [stringTopTracks, setStringTopTracks] = useState('');
-
-  useEffect(()=> {
-    async function getUserTopTracks() {
-      if (auth.id){
-       return await getTopTracks();
-      }
-    }
-    setTopTracks(getUserTopTracks());
-
-  }, [auth]);
-
-  useEffect(()=> {
-    async function setJson(){
-      if (await topTracks != ''){
-        topTracks.then(function(result) {
-          let str = '';
-          result.data.items.forEach( track => {
-            if (str.length < 5){
-              str += `"${track.name}" by ${track.artists[0].name} from ${track.album.name}`;
-            }
-            else{
-              str += `, "${track.name}" by ${track.artists[0].name} from ${track.album.name}`;
-            }
-          })
-          setStringTopTracks(str);
-        });
-      }
-    }
-    setJson();
-
-  }, [topTracks]);
+// const Prompt = () => {
+//   const dispatch = useDispatch();
+//   const { prompt, auth, playlists } = useSelector(state => state);
+//   const { jsonResponse } = useSelector(state => state.prompt);
+//   const [input, setInput] = useState('');
+//   const [topTracks, setTopTracks] = useState('');
+//   const [testClicked, setTestClicked] = useState(false);
+//   const [showExamplePrompts, setShowExamplePrompts] = useState(true);
+//   const [isLoading, setIsLoading] = useState(false);
+//   const [stringTopTracks, setStringTopTracks] = useState('');
+//   const [selectedItems, setSelectedItems] = useState([]);
 
 
+//   useEffect(() => {
+//     async function getUserTopTracks() {
+//       if (auth.id) {
+//         const response = await getTopTracks();
+//         setTopTracks(response.data.items.map((track) => track));
+//       }
+//     }
+//     getUserTopTracks();
+//   }, [auth]);
 
-  const submit = (ev) => {
-    ev.preventDefault();
-    dispatch(getResponse(input));
-  }
+//   const submit = async (ev) => {
+//     ev.preventDefault();
+//     setIsLoading(true);
+//     await dispatch(getJSONResponse('songs that fit the following criteria', 5, input, auth.discoverPlaylists));
+//     setIsLoading(false);
+//     setTestClicked(true);
+//     setShowExamplePrompts(false);
+//   };
+
+//   const goBack = () => {
+//     setShowExamplePrompts(true);
+//     setTestClicked(false);
+//   };
+
+//   useEffect(() => {
+//     const setJson = async () => {
+//       if (topTracks && topTracks.length > 0) {
+//         let str = '';
+//         topTracks.forEach((track) => {
+//           if (str.length < 5) {
+//             str += `"${track.name}" by ${track.artists[0].name} from ${track.album.name}`;
+//           } else {
+//             str += `, "${track.name}" by ${track.artists[0].name} from ${track.album.name}`;
+//           }
+//         });
+//         setStringTopTracks(str);
+//       }
+//     };
+
+//     setJson();
+//   }, [topTracks]);
+
+//   const selectPromptOption = (text) => {
+//     setInput(text);
+//   };
+
+//   const toggleItemSelection = (index) => {
+//     setSelectedItems((prevSelectedItems) => {
+//       if (prevSelectedItems.includes(index)) {
+//         return prevSelectedItems.filter((item) => item !== index);
+//       } else {
+//         return [...prevSelectedItems, index];
+//       }
+//     });
+//   };
+
+//   const handleGeneratePlaylist = () => {
+//     const selectedResponses = selectedItems.map((index) => jsonResponse[index]);
+//     console.log(selectedResponses);
+//     dispatch(getSpotifyURIs(selectedResponses));
+//   };
+
+//   return (
+//     <div className='prompt-container'>
+//       <form onSubmit={submit}>
+//         <input className="prompt-input" value={input} onChange={(ev) => { setInput(ev.target.value) }}></input>
+//         <button className="StyledLogoutButton">Test</button>
+//       </form>
+
+//       {isLoading ? (
+//         <Loader />
+//       ) : (
+//         <>
+
+// {testClicked && Array.isArray(jsonResponse) && jsonResponse.length > 0 && (
+//   <div className="playlist-container">
+//     <h2 className="playlist-header">Playlist</h2>
+//     {jsonResponse.map((response, index) => (
+//       <div className="playlist-item" key={index}>
+//         <div className="playlist-item-info">
+//           <div className="playlist-item-row">
+//             <div className="playlist-item-title">{response.title}</div>
+//           </div>
+//           <div className="playlist-item-artist">{response.artist}</div>
+//           <div className="playlist-item-checkbox">
+//             <input
+//               type="checkbox"
+//               checked={selectedItems.includes(index)}
+//               onChange={() => toggleItemSelection(index)}
+//             />
+//           </div>
+//         </div>
+//       </div>
+//     ))}
+//     <div className="playlist-buttons-container">
+//       <button className="playlist-back-button" onClick={goBack}>
+//         Back
+//       </button>
+//       <button
+//         className="playlist-generate-button"
+//         onClick={() => handleGeneratePlaylist()}
+//       >
+//         Generate Playlist
+//       </button>
+//     </div>
+//   </div>
+// )}
 
 
-  return(
-    <div className='prompt-Container'>
-      <form  onSubmit={submit}>
-        <input onChange={ (ev) => { setInput(ev.target.value)}}></input>
-        <button className="StyledLogoutButton" >Test</button>
-      </form>
-      <div className='prompt-Element'>
-      <button onClick={()=> {dispatch(getResponse('Give me a playlist of ten random popular songs on spotify.'))}}>Generate Random Playlist</button>
-      <button onClick={()=> {dispatch(getResponse('List ten random popular artists on spotify.'))}}>Find Artists</button>
-      <button onClick={async()=> {await dispatch(getJSONResponse('similar songs to the following playlist', 5, stringTopTracks)); }}>Given JSON</button>
-      </div>
+//           {showExamplePrompts && (
+//             <>
+//               <h2 className="options-title">Example Prompts</h2>
+//               <div className="options with-arrow" onClick={() => selectPromptOption('Music for chill relaxing vibes')}>
+//                 <span className="pl-type-desc">Music for chill relaxing vibes</span>
+//               </div>
+//               <div className="options with-arrow" onClick={() => selectPromptOption('Top 10 artists from the 1990s')}>
+//                 <span className="pl-type-desc">Top 10 artists from the 1990s</span>
+//               </div>
+//               <div className="options with-arrow" onClick={() => selectPromptOption('Music for a beach party')}>
+//                 <span className="pl-type-desc">Music for a beach party</span>
+//               </div>
+//               <div className="options with-arrow" onClick={() => selectPromptOption('Dinner Party tunes')}>
+//                 <span className="pl-type-desc">Dinner Party tunes</span>
+//               </div>
+//               <div className="options with-arrow" onClick={() => selectPromptOption('Grad party playlist!')}>
+//                 <span className="pl-type-desc">Grad party playlist!</span>
+//               </div>
+//             </>
+//           )}
+//         </>
+//       )}
+//     </div>
+//   )
+// };
 
-      <div className='messages'>
-        {
-           prompt.map(_prompt => {
-              return <div key={_prompt.id} id={_prompt.id}>
-                    <div className='userMessage'> { _prompt.userPrompt}</div>
-                     <div className='chatgptMessage'>{_prompt.response}</div>
-                     </div>
-           })
-        }
+// export default Prompt;
 
-
-      </div>
-      
-    </div>
-  )
-};
-export default Prompt;
-
-*/
