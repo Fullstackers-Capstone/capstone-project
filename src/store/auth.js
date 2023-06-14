60ff76eaafaf1308b8e053267e3157dd3caa5f92
“Brian <brianshanley242@gmail.com>
2023-06-14 19:52:56 -0400

    Auth error handling done

--- src/store/auth.js
+++ src/store/auth.js
@@ -5,46 +5,68 @@ const auth = (state = { }, action)=> {
   if(action.type === 'SET_AUTH'){
     return action.auth;
   }
+  if(action.type === 'SERVER_ERROR'){
+    return {...state, error: action.payload};
+  }
   return state;
 };
 
 export const fetchSpotUser = () => {
   return async(dispatch) => {
-    const user = await getCurrentUserProfile();
+    try {
+      const user = await getCurrentUserProfile();
       const response = await axios.get(`/api/auth/${user.data.id}/`);
-      
+
       localStorage.setItem("spotifyId",response.data.spotifyId);
 
       createDbUser(response);
 
-      dispatch({ type: 'SET_AUTH', auth: response.data})
+      dispatch({ type: 'SET_AUTH', auth: response.data});
+    } catch (error) {
+      console.error(error);
+      dispatch({type: 'SERVER_ERROR', payload: "Error fetching Spotify user!"});
+    }
   }
 }
 
 export const createDbUser = (response) => {
-
   return async(dispatch) => {
-    await axios.post('api/users', {
-      spotifyId: response.data.id,
-      display_name: response.data.display_name,
-      email: response.data.email,
-      image: response.data.images[0].url
-    })
+    try {
+      await axios.post('api/users', {
+        spotifyId: response.data.id,
+        display_name: response.data.display_name,
+        email: response.data.email,
+        image: response.data.images[0].url
+      })
+    } catch (error) {
+      console.error(error);
+      dispatch({type: 'SERVER_ERROR', payload: "Error creating user in the database!"});
+    }
   }
 }
 
 export const updateAuth = (user)=> {
   return async(dispatch)=> {
-    const response = await axios.put(`/api/users/${user.id}`, user);
-    dispatch({ type: 'SET_AUTH', auth: response.data });
+    try {
+      const response = await axios.put(`/api/users/${user.id}`, user);
+      dispatch({ type: 'SET_AUTH', auth: response.data });
+    } catch (error) {
+      console.error(error);
+      dispatch({type: 'SERVER_ERROR', payload: "Error updating auth!"});
+    }
   }
 }
 
 export const upgradeToPro = (user) => {
   return async (dispatch) => {
-    const request = {spotifyId: user}
+    try {
+      const request = {spotifyId: user}
       const response = await axios.put('/api/users/upgradeToPro', request);
       dispatch({ type: 'SET_AUTH', auth: response.data });
+    } catch (error) {
+      console.error(error);
+      dispatch({type: 'SERVER_ERROR', payload: "Error upgrading to pro!"});
+    }
   };
 };
 
