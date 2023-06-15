a5cb9d5d441952e0199659ff8b0fae4478d995dc
“Brian <brianshanley242@gmail.com>
2023-06-14 20:09:45 -0400

    Prompt slice error handling done

--- src/store/prompt.js
+++ src/store/prompt.js
@@ -46,80 +46,109 @@ export const updatePrompt = (prompt) => ({
 })
 
 export const getResponse = (prompt) => {
-  
   return async (dispatch) => {
-    const request = { prompt: prompt };
-    const spotifyId = window.localStorage.getItem('spotifyId');
-    const response = await axios.post('/api/prompt', request, {
-      headers: {
-        spotifyId: spotifyId
-      }
-    });
-    dispatch(setPrompt(response.data));
+    try {
+      const request = { prompt: prompt };
+      const spotifyId = window.localStorage.getItem('spotifyId');
+      const response = await axios.post('/api/prompt', request, {
+        headers: {
+          spotifyId: spotifyId
+        }
+      });
+      dispatch(setPrompt(response.data));
+    } catch (error) {
+      console.error(error);
+      dispatch({type: SERVER_ERROR, payload: "Error getting response!"});
+    }
   };
 };
 
 export const getJSONResponse = (prompt, length, data) => {
   return async (dispatch) => {
-    const request = {prompt: prompt,length: length, spotifyData: data};
-    const spotifyId = window.localStorage.getItem('spotifyId');
-    const response = await axios.post('/api/prompt/json', request, {
-      headers: {
-        spotifyId: spotifyId
-      }
-    });
-    dispatch(savePrompt(response.data));
+    try {
+      const request = {prompt: prompt,length: length, spotifyData: data};
+      const spotifyId = window.localStorage.getItem('spotifyId');
+      const response = await axios.post('/api/prompt/json', request, {
+        headers: {
+          spotifyId: spotifyId
+        }
+      });
+      dispatch(savePrompt(response.data));
+    } catch (error) {
+      console.error(error);
+      dispatch({type: 'SERVER_ERROR', payload: "Error getting JSON response!"});
+    }
   };
 };
 
 export const getSpotifyURIs = (prompt, playlist) => {
   return async (dispatch) => {
-    const URIResponse = await Promise.all(playlist.map(async(element) => {
-      const uri = await searchFunctionality(element)
-      if (await uri){
-        return await uri;
-      }
-    }));
-    // Log the final URIResponse
-    const filteredResponse = URIResponse.filter(uri => uri !== undefined)
-    //await createPlaylist({userId: spotifyId, name: 'Anything We Want', description: 'Same with the description.'}, filteredResponse)
-    prompt.uriList = filteredResponse;
-    dispatch(update(prompt));
+    try {
+      const URIResponse = await Promise.all(playlist.map(async(element) => {
+        const uri = await searchFunctionality(element)
+        if (await uri){
+          return await uri;
+        }
+      }));
+      // Log the final URIResponse
+      const filteredResponse = URIResponse.filter(uri => uri !== undefined)
+      //await createPlaylist({userId: spotifyId, name: 'Anything We Want', description: 'Same with the description.'}, filteredResponse)
+      prompt.uriList = filteredResponse;
+      dispatch(update(prompt));
+    } catch (error) {
+      console.error(error);
+      dispatch({type: 'SERVER_ERROR', payload: "Error getting Spotify URIs!"});
+    }
   }
 }
 
 
 export const savePrompt = (prompt) => {
   return async (dispatch) => {
-    const request = { prompt: prompt };
-    const spotifyId = window.localStorage.getItem('spotifyId');
-    const response = await axios.put('/api/prompt', request, {
-      headers: {
-        spotifyId: spotifyId
-      }
-    });
-    dispatch(createPrompt(response.data));
+    try {
+      const request = { prompt: prompt };
+      const spotifyId = window.localStorage.getItem('spotifyId');
+      const response = await axios.put('/api/prompt', request, {
+        headers: {
+          spotifyId: spotifyId
+        }
+      });
+      dispatch(createPrompt(response.data));
+    } catch (error) {
+      console.error(error);
+      dispatch({type: SERVER_ERROR, payload: "Error saving prompt!"});
+    }
   };
 };
 
 export const update = (prompt) => {
   return async (dispatch) => {
-    const request = { prompt: prompt };
-    const response = await axios.put('/api/prompt', request);
-    dispatch(updatePrompt(response.data));
+    try {
+      const request = { prompt: prompt };
+      const response = await axios.put('/api/prompt', request);
+      dispatch(updatePrompt(response.data));
+    } catch (error) {
+      console.error(error);
+      dispatch({type: SERVER_ERROR, payload: "Error updating prompt!"});
+    }
   };
 };
 
 
 export const getAllPrompts = () => {
   return async (dispatch) => {
-    const spotifyId = window.localStorage.getItem('spotifyId');
-    const response = await axios.get('/api/prompt', {
-      headers: {
-        spotifyId: spotifyId
-      }
-    });
-    dispatch(setPrompt(response.data));
+    try {
+      const spotifyId = window.localStorage.getItem('spotifyId');
+      const response = await axios.get('/api/prompt', {
+        headers: {
+          spotifyId: spotifyId
+        }
+      });
+      dispatch(setPrompt(response.data));
+    } catch (error) {
+      console.error(error);
+      dispatch({type: SERVER_ERROR, payload: "Error fetching all prompts!"});
+    }
   };
 };
 
