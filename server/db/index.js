7e908a21628a97cc939a38f0c54c6ce5b1a0df7b
Ryan Sariego <rsariego@fastmail.com>
2023-06-20 17:22:50 -0400

    minor css changes

--- server/db/index.js
+++ server/db/index.js
@@ -9,18 +9,7 @@ User.hasMany(Playlist);
 User.hasMany(Prompt);
 const syncAndSeed = async()=> {
   await conn.sync({force: true});
-  // const [moe, lucy, larry, ethyl] = await Promise.all([
-  //   User.create({ username: 'moe', password: '123'}),
-  //   User.create({ username: 'lucy', password: '123' }),
-  //   User.create({ username: 'larry', password: '123' }),
-  // ]);
-  // return {
-  //   users: {
-  //     moe,
-  //     lucy,
-  //     larry
-  //   }
-  // };
+
 };
 module.exports = {
   syncAndSeed,
