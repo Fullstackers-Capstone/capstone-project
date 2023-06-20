0637c8ea14206ef33ca24b3e844f24e89d5b09b3
Mauricio Toscano <toscano95@gmail.com>
2023-06-20 11:18:17 -0400

    final changes

--- server/index.js
+++ server/index.js
@@ -7,11 +7,9 @@ catch(ex){
   console.log(ex);
 }
 const app = require('./app');
-const { syncAndSeed } = require('./db');
 
 const init = async()=> {
   try {
-    await syncAndSeed();
     const port = process.env.PORT || 3000;
     const server = app.listen(port, ()=> console.log(`listening on port ${port}`));
   }
